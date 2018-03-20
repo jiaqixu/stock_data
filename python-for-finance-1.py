9e26f4cb37de226519f9141be1a20e7f3293bf39
jiaqi <jiaqiperson@hotmail.com>
2018-03-19 22:07:58 -0500

    tutorial up to 4

--- python-for-finance-1.py
+++ python-for-finance-1.py
@@ -1,6 +1,9 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+import mpl_finance
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 from pandas_datareader import data as pdr
@@ -15,7 +18,7 @@ yf.pdr_override() # <== that's all it takes :-)
 #data = pdr.get_data_yahoo(["SPY", "IWM"], start="2017-01-01", end="2017-04-30")
 '''
 style.use('ggplot')
-start=dt.datetime(2000,1,1)
+start=dt.datetime(2010,6,1)
 end=dt.datetime(2016,12,31)
 
 df=pdr.get_data_yahoo('TSLA',start,end)
@@ -27,15 +30,32 @@ df=pd.read_csv('tsla.csv',parse_dates=True,index_col=0)
 #df['Adj Close'].plot()
 #plt.show()
 #print(df[['Open','High']].head())
-df['100ma']=df['Adj Close'].rolling(window=100,min_periods=0).mean()
+#df['100ma']=df['Adj Close'].rolling(window=100,min_periods=0).mean()
 #df.dropna(inplace=True)
-print(df.head())
+#print(df.head())
+
+###resample data
+###resample data to every 10 days
+df_ohlc=df['Adj Close'].resample('10D').ohlc()
+df_volume=df['Volume'].resample('10D').sum()
+df_ohlc.reset_index(inplace=True)
+#print(df_ohlc.head())
+df_ohlc['Date']=df_ohlc['Date'].map(mdates.date2num)
+#print(df_ohlc.head())
+
+
+
 
 ax1=plt.subplot2grid((6,1),(0,0),rowspan=5,colspan=1)
 ax2=plt.subplot2grid((6,1),(5,0),rowspan=1,colspan=1,sharex=ax1)
+ax1.xaxis_date()
 
+candlestick_ohlc(ax1,df_ohlc.values,width=2,colorup='g')
+ax2.fill_between((df_volume.index).map(mdates.date2num),df_volume.values,0)
+'''
 ax1.plot(df.index,df['Adj Close'])
 ax2.plot(df.index,df['100ma'])
 ax2.bar(df.index,df['Volume'])
+'''
 
 plt.show()
