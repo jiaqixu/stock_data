559ad8d42f04db5232b75745f7f383f8a6656937
jiaqi <jiaqiperson@hotmail.com>
2018-03-20 11:04:32 -0500

    download 500 S&P stocks

--- python-for-finance-2.py
+++ python-for-finance-2.py
@@ -41,7 +41,7 @@ def get_data_from_yahoo(reload_sp500=False):
     if not os.path.exists('stock_dfs'):
         os.makedirs('stock_dfs')
 
-    start=dt.datetime(2016,1,1)      
+    start=dt.datetime(2000,1,1)      
     end=dt.datetime(2016,12,31)
 
     for ticker in tickers:
